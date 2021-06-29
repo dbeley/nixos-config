ed8b75aac998d94133486ec0982e0a726049ec10
dbeley <6568955+dbeley@users.noreply.github.com>
2021-06-29 15:06:36 +0200

    Update.

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-Chuwi"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
 
   # Set your time zone.
