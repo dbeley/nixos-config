95dc3bc29bb49b1c8997d0017b12b37169724182
dbeley <6568955+dbeley@users.noreply.github.com>
2023-01-29 23:33:39 +0100

    Fix waybar workspaces

---
+++ hosts/apps/waybar/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ...}:
+
+{
+  environment.systemPackages = with pkgs; [
+    waybar
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        patchPhase = ''
+          substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"hyprctl dispatch workspace \" + name_; system(command.c_str());"
+        '';
+      });
+    })
+  ];
+}
--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./apps/steam/steam.nix
+      ./apps/waybar/default.nix
     ];
 
   # Bootloader.
