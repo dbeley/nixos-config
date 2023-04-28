0bef4ef324e75b220af9c007cdfa820a5334bf08
dbeley <6568955+dbeley@users.noreply.github.com>
2023-04-28 12:30:08 +0200

    Update emacs, lint with alejandra

--- hosts/apps/emacs/emacs.nix
+++ hosts/apps/emacs/emacs.nix
@@ -1,7 +1,12 @@
-{nix-doom-emacs, ...}: {
+{
+  nix-doom-emacs,
+  pkgs,
+  ...
+}: {
   imports = [nix-doom-emacs.hmModule];
   programs.doom-emacs = {
     enable = true;
     doomPrivateDir = ./doom.d;
   };
+  home.packages = with pkgs; [pandoc];
 }
