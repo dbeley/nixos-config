320b17fb0ec10d380cae07ed61438245d713d3e0
dbeley <6568955+dbeley@users.noreply.github.com>
2023-06-29 21:14:34 +0200

    Move to nixpkgs-unstable channel

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
