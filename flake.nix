c48356eef2e4f9add238d01adcf5ada34c79e48b
dbeley <6568955+dbeley@users.noreply.github.com>
2023-07-28 22:44:44 +0200

    Fix binary cache, update font config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
 {
   description = "NixOS configuration";
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org/"
+    ];
+
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -84,7 +84,7 @@
     wireguard-tools
   ];
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["Iosevka"];})
     eb-garamond
     liberation_ttf
@@ -144,18 +144,6 @@
     };
     registry.nixpkgs.flake = inputs.nixpkgs;
     nixPath = ["nixpkgs=/etc/channels/nixpkgs"];
-    settings = {
-      builders-use-substitutes = true;
-      substituters = [
-        "https://cache.nixos.org/"
-      ];
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
   };
 
   environment.etc."channels/nixpkgs".source = inputs.nixpkgs.outPath;
