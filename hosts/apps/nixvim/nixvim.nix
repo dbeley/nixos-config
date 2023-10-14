74f77c10e468cbf5785c934bd17c9f6a59d41c19
dbeley <6568955+dbeley@users.noreply.github.com>
2023-10-14 23:38:47 +0200

    Update nixvim: add plugins

--- hosts/apps/nixvim/nixvim.nix
+++ hosts/apps/nixvim/nixvim.nix
@@ -16,6 +16,43 @@
 	theme = "minimalist";
       };
       telescope.enable = true;
+      alpha.enable = true;
+      bufferline.enable = true;
+
+      nvim-cmp.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-path.enable = true;
+      cmp-rg.enable = true;
+
+      comment-nvim.enable = true;
+      diffview.enable = true;
+      fugitive.enable = true;
+      gitsigns.enable = true;
+      hardtime = {
+        enable = true;
+	enabled = true;
+      };
+      leap.enable = true;
+      lsp = {
+        enable = true;
+	servers = {
+	  bashls.enable = true;
+          html.enable = true;
+	  nil_ls.enable = true;
+	  pylsp.enable = true;
+	};
+      };
+      lsp-format.enable = true;
+      markdown-preview.enable = true;
+      mini.enable = true;
+      navbuddy.enable = true;
+      neo-tree.enable = true;
+      notify.enable = true;
+      sniprun.enable = true;
+      surround.enable = true;
+
+      trouble.enable = true;
+      which-key.enable = true;
     };
 
     globals.mapleader = ";"; # Sets the leader key to comma
