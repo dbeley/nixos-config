ce327b0f3824c367a0675390b7a38500d3a47704
dbeley <6568955+dbeley@users.noreply.github.com>
2023-10-15 01:18:33 +0200

    Update nixvim

--- hosts/apps/nixvim/nixvim.nix
+++ hosts/apps/nixvim/nixvim.nix
@@ -9,55 +9,16 @@
       };
     };
 
-    keymaps = [
-      {
-        key = "<C-s>";
-	action = ":w<CR>";
-      }
-      {
-        key = "<esc>";
-	action = ":noh<CR>";
-	options = {
-	  silent = true;
-	};
-      }
-      {
-        key = "<leader>f";
-	action = "<cmd>Telescope find_files<cr>";
-      }
-      {
-        key = "<leader>g";
-	action = "<cmd>Telescope live_grep<cr>";
-      }
-      {
-        key = "<leader>b";
-	action = "<cmd>Telescope buffers<cr>";
-      }
-      {
-        key = "<leader>t";
-	action = "<cmd>Telescope help_tags<cr>";
-      }
-      {
-        key = "<leader>sr";
-	action = "<cmd>SnipRun<cr>";
-      }
-      {
-        key = "<leader>ss";
-	action = "<cmd>'<,'>SnipRun<cr>";
-      }
-      {
-        key = "<leader>sd";
-	action = "<cmd>SnipReset<cr>";
-      }
-      {
-        key = "<leader>sc";
-	action = "<cmd>SnipClose<cr>";
-      }
-      {
-        key = "<leader>n";
-	action = "<cmd>NeoTree<cr>";
-      }
-    ];
+    globals.mapleader = ";"; # Sets the leader key to comma
+
+    options = {
+      number = true;         # Show line numbers
+      relativenumber = false; # Show relative line numbers
+      incsearch = true;
+      shiftwidth = 2;        # Tab width should be 2
+      tabstop = 2;
+      termguicolors = true;
+    };
 
     plugins = {
       airline = {
@@ -77,7 +38,7 @@
       leap.enable = true;
       lsp-format.enable = true;
       markdown-preview.enable = true;
-      mini.enable = true;
+      # mini.enable = true;
       navbuddy.enable = true;
       neo-tree.enable = true;
       notify.enable = true;
@@ -105,13 +66,70 @@
       cmp-rg.enable = true;
     };
 
-    globals.mapleader = ";"; # Sets the leader key to comma
-
-    options = {
-      number = true;         # Show line numbers
-      relativenumber = false; # Show relative line numbers
-      incsearch = true;
-      shiftwidth = 2;        # Tab width should be 2
-    };
+    keymaps = [
+      {
+        key = "<C-s>";
+	action = ":w<CR>";
+      }
+      {
+        key = "<esc>";
+	action = ":noh<CR>";
+	options = {
+	  silent = true;
+	};
+      }
+      {
+        key = "<leader>k";
+	action = "<cmd>bdelete<cr>";
+      }
+      {
+        key = "<leader>f";
+	action = "<cmd>Telescope find_files<cr>";
+      }
+      {
+        key = "<leader>g";
+	action = "<cmd>Telescope live_grep<cr>";
+      }
+      {
+        key = "<leader>b";
+	action = "<cmd>Telescope buffers<cr>";
+      }
+      {
+        key = "<leader>t";
+	action = "<cmd>Telescope help_tags<cr>";
+      }
+      {
+        key = "<leader>nn";
+	action = "<cmd>Neotree<cr>";
+      }
+      {
+        key = "<leader>nb";
+	action = "<cmd>Neotree buffers<cr>";
+      }
+      {
+        key = "<leader>ng";
+	action = "<cmd>Neotree float git_status<cr>";
+      }
+      {
+        key = "<leader>nc";
+	action = "<cmd>Neotree close<cr>";
+      }
+      {
+        key = "<leader>sr";
+	action = "<cmd>SnipRun<cr>";
+      }
+      {
+        key = "<leader>ss";
+	action = "<cmd>'<,'>SnipRun<cr>";
+      }
+      {
+        key = "<leader>sd";
+	action = "<cmd>SnipReset<cr>";
+      }
+      {
+        key = "<leader>sc";
+	action = "<cmd>SnipClose<cr>";
+      }
+    ];
   };
 }
