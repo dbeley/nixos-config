599f4de187f3a6f561d0bfe4388dae17a524ada2
dbeley <6568955+dbeley@users.noreply.github.com>
2023-10-18 13:46:04 +0200

    Add lf

---
+++ hosts/apps/lf/lf.nix
@@ -0,0 +1,15 @@
+{
+  xdg.configFile."lf/icons".source = ./icons;
+
+  programs.lf= {
+    enable = true;
+
+    settings = {
+        preview = true;
+        hidden = true;
+        drawbox = true
+        icons = true;
+        ignorecase = true;
+    };
+  };
+}
--- hosts/t470s/home.nix
+++ hosts/t470s/home.nix
@@ -22,6 +22,7 @@
     ../apps/emacs/emacs.nix
     ../apps/kakoune/kakoune.nix
     ../apps/helix/helix.nix
+    ../apps/lf/lf.nix
     ../apps/wal/wal.nix
     ../apps/wpgtk/wpgtk.nix
     ../apps/udiskie/udiskie.nix
