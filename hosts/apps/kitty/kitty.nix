1b3f45d4566eb9cdf8a7304fa95742f684660441
dbeley <6568955+dbeley@users.noreply.github.com>
2023-11-29 20:24:39 +0100

    Fix kitty font

--- hosts/apps/kitty/kitty.nix
+++ hosts/apps/kitty/kitty.nix
@@ -2,10 +2,10 @@
   programs.kitty = {
     enable = true;
     settings = {
-      font_family = "Ioveska Nerd Font Mono";
-      font_size = "11";
+      font_family = "Iosevka Nerd Font";
+      font_size = "12";
       background_opacity = "0.8";
-      window_padding_width = 9;
+      window_padding_width = "10 10";
       cursor_shape = "block";
       shell_integration = "no-cursor";
       };
