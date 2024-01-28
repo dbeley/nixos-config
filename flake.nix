e281667a2d73413cb7276da334d040a5df620552
dbeley <6568955+dbeley@users.noreply.github.com>
2024-01-28 23:43:28 +0100

    Use nixos-unstable channel

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
