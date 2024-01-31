f2376e89b7065e40f18d5c3b030704a1f1438104
dbeley <6568955+dbeley@users.noreply.github.com>
2024-01-31 11:23:49 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     disko = {
       url = "github:nix-community/disko";
