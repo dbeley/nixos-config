1832fb62450daef302ec6c8b5b7b1b312c925b17
dbeley <6568955+dbeley@users.noreply.github.com>
2024-03-03 21:10:14 +0100

    Add hyprland cache, disable channels

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -131,11 +131,15 @@
       keep-derivations = true
     '';
     settings = {
-      substituters =
-        [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+      ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
     gc = {
@@ -144,6 +148,7 @@
       options = "--delete-older-than 7d";
     };
     registry.nixpkgs.flake = inputs.nixpkgs;
+    channel.enable = false;
     nixPath = [ "nixpkgs=/etc/channels/nixpkgs" ];
   };
 
