6e7b95db140e80fd651a6a01c0961bfa50135a17
dbeley <6568955+dbeley@users.noreply.github.com>
2024-03-09 23:41:48 +0100

    nix fmt

--- hosts/x13/hardware-configuration.nix
+++ hosts/x13/hardware-configuration.nix
@@ -4,26 +4,26 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/50e45ddd-ed7a-4dea-9733-8e457fb9381e";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/50e45ddd-ed7a-4dea-9733-8e457fb9381e";
+    fsType = "ext4";
+  };
 
-  boot.initrd.luks.devices."luks-29a626e7-bc1f-4e11-871d-bc08af841712".device = "/dev/disk/by-uuid/29a626e7-bc1f-4e11-871d-bc08af841712";
+  boot.initrd.luks.devices."luks-29a626e7-bc1f-4e11-871d-bc08af841712".device =
+    "/dev/disk/by-uuid/29a626e7-bc1f-4e11-871d-bc08af841712";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/A021-2913";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/A021-2913";
+    fsType = "vfat";
+  };
 
   swapDevices = [ ];
 
@@ -37,5 +37,6 @@
   # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
