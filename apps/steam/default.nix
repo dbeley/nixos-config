3fc56efcf56701b9faa8593cfa5dbf80790d1743
dbeley <6568955+dbeley@users.noreply.github.com>
2024-03-15 15:47:43 +0100

    steam: add gamescope

--- apps/steam/default.nix
+++ apps/steam/default.nix
@@ -3,5 +3,6 @@
     enable = true;
     # remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
     # dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
   };
 }
