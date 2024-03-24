24ce669a191e7bb33cbfbd245fad088414dab72a
dbeley <6568955+dbeley@users.noreply.github.com>
2024-03-24 10:08:48 +0100

    Use powertop instead of tlp in laptop.nix

--- modules/common/laptop.nix
+++ modules/common/laptop.nix
@@ -3,6 +3,7 @@
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  services.tlp.enable = true;
+  powerManagement.powertop.enable = true;
+  # services.tlp.enable = true;
   services.logind.lidSwitch = "ignore";
 }
