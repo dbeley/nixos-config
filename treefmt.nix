1cf89ab5775bb9b812ba2974c6bad16ee58e08c8
dbeley <6568955+dbeley@users.noreply.github.com>
2024-04-04 00:09:09 +0200

    treefmt: use nixfmt-rfc-style

--- treefmt.nix
+++ treefmt.nix
@@ -2,6 +2,6 @@
   # Used to find the project root
   projectRootFile = "flake.nix";
   # programs.alejandra.enable = true;
-  programs.nixfmt.enable = true;
+  programs.nixfmt-rfc-style.enable = true;
   programs.statix.enable = true;
 }
