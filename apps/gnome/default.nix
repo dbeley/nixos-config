74fc28015b8519f641c3d37b08b4d15fe743d6da
dbeley <6568955+dbeley@users.noreply.github.com>
2024-04-27 16:42:01 +0200

    gnome: rename deprecated options

--- apps/gnome/default.nix
+++ apps/gnome/default.nix
@@ -1,15 +1,17 @@
 { user, ... }:
 {
+  services.displayManaer = {
+    defaultSession = "gnome";
+    autoLogin = {
+      enable = true;
+      user = "${user}";
+    };
+  };
   services.xserver = {
     enable = true;
     desktopManager.gnome.enable = true;
     displayManager = {
       gdm.enable = true;
-      defaultSession = "gnome";
-      autoLogin = {
-        enable = true;
-        user = "${user}";
-      };
     };
   };
 
