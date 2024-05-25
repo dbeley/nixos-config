26ef38abd85a1def4c40714a8ed7ede462050b9c
dbeley <6568955+dbeley@users.noreply.github.com>
2024-05-26 01:42:31 +0200

    steam: update mangohud config

--- apps/steam/steam.nix
+++ apps/steam/steam.nix
@@ -1,4 +1,20 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [ mangohud ];
+  programs.mangohud = {
+    enable = true;
+    enableSessionWide = true;
+    settings = {
+      horizontal = true;
+      battery = true;
+      cpu_stats = true;
+      gpu_stats = true;
+      cpu_power = true;
+      gpu_power = true;
+      ram = true;
+      fps = true;
+      table_columns = 14;
+      frametime = 0;
+      frame_timing = 1;
+      hud_no_margin = true;
+    };
+  };
 }
