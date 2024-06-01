3f0cf821b1ece2cb5d67d165b74f6a2bc5aec472
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-02 00:28:09 +0200

    stylix: add new wallpaper

--- apps/stylix/default.nix
+++ apps/stylix/default.nix
@@ -5,10 +5,15 @@
     #   url = "https://images.unsplash.com/photo-1672491441167-1daa7c3c95a8";
     #   sha256 = "WoYwXx6WT+mzAmnsY+eY+eCMzf+PwMApUQuOTNaIjGE=";
     # };
+    # image = pkgs.fetchurl {
+    #   url = "https://images.unsplash.com/photo-1542052605271-43cf9ff1609f";
+    #   sha256 = "aR1JAQLBisLuz2Gd9ARV8qW0ePQqMpUiLq7teth2p4Q=";
+    # };
     image = pkgs.fetchurl {
-      url = "https://images.unsplash.com/photo-1542052605271-43cf9ff1609f";
-      sha256 = "aR1JAQLBisLuz2Gd9ARV8qW0ePQqMpUiLq7teth2p4Q=";
+      url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/HK%20P9162201.jpg?raw=true";
+      sha256 = "+l73wncx2U79NQmBzjP0yVzUENqopIFjNzfOKTBxe2Q=";
     };
+
 
     polarity = "dark";
     fonts = {
