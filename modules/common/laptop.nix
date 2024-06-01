2595f22ef28f30e7f63b55a19848f8f51a4e6e28
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-01 22:46:33 +0200

    disable tlp, disable blueman overlay

--- modules/common/laptop.nix
+++ modules/common/laptop.nix
@@ -1,19 +1,19 @@
 {
   imports = [ ../../apps/light/light.nix ];
 
-  nixpkgs.overlays = [
-    (self: super: {
-      blueman = super.blueman.overrideAttrs (oldAttrs: {
-        version = "2.3.5";
-        src = super.fetchurl {
-          url = "https://github.com/blueman-project/blueman/releases/download/2.3.5/blueman-2.3.5.tar.xz";
-          sha256 = "sha256-stIa/fd6Bs2G2vVAJAb30qU0WYF+KeC+vEkR1PDc/aE=";
-        };
-      });
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     blueman = super.blueman.overrideAttrs (oldAttrs: {
+  #       version = "2.3.5";
+  #       src = super.fetchurl {
+  #         url = "https://github.com/blueman-project/blueman/releases/download/2.3.5/blueman-2.3.5.tar.xz";
+  #         sha256 = "sha256-stIa/fd6Bs2G2vVAJAb30qU0WYF+KeC+vEkR1PDc/aE=";
+  #       };
+  #     });
+  #   })
+  # ];
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-  services.tlp.enable = true;
+  # services.tlp.enable = true;
   services.logind.lidSwitch = "ignore";
 }
