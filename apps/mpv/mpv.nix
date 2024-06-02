f3697b9538e34a14e4f91ce841fd3f0926e027ee
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-02 11:00:49 +0200

    mpv: add plugins

--- apps/mpv/mpv.nix
+++ apps/mpv/mpv.nix
@@ -1,5 +1,12 @@
+{ pkgs, ... }:
 {
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [
+      thumbfast
+      uosc
+      # sponsorblock
+      # mpv-webm
+    ];
   };
 }
