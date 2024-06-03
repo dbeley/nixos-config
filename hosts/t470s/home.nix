47aac9bb2abbd78b62129319ccddf1bd09529628
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-03 21:52:00 +0200

    hyprland: disable flake

--- hosts/t470s/home.nix
+++ hosts/t470s/home.nix
@@ -7,7 +7,7 @@
 }:
 {
   imports = [
-    inputs.hyprland.homeManagerModules.default
+    # inputs.hyprland.homeManagerModules.default
     ../../apps/hyprland/hyprland.nix
     ../../apps/waybar/waybar.nix
     ../../apps/tofi/tofi.nix
