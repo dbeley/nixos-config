d490e25605681e3ce1c7ccbe0f896a3a3abb8709
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-04 19:33:08 +0200

    hyprland: use mako & hyprland service

--- apps/hyprland/hyprland.nix
+++ apps/hyprland/hyprland.nix
@@ -7,9 +7,7 @@
 }:
 {
   home.packages = with pkgs; [
-    mako
     libnotify
-    hyprpaper
     grim
     slurp
     pamixer
@@ -22,21 +20,25 @@
       rev = "a8607fbfb8c50543629e14ec483473459229091d";
       sha256 = "XBumWlu4+z/jTKLK71Lr0hMeLhc43XD3oEzY+YUMzN4=";
     };
-    ".config/hypr/hyprpaper.conf" = {
-      text = ''
-        preload=${config.stylix.image}
-        wallpaper=eDP-1,${config.stylix.image}
-        wallpaper=DP-3,${config.stylix.image}
-        wallpaper=DP-5,${config.stylix.image}
-        wallpaper=HDMI-A-1,${config.stylix.image}
-        splash=false
-      '';
-    };
   };
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
     extraConfig = lib.fileContents ./hyprland.conf;
     # plugins = [ inputs.hyprspace.packages.${pkgs.system}.Hyprspace ];
+  };
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = [
+        config.stylix.image
+      ];
+      wallpaper = [
+        "eDP-1,${config.stylix.image}"
+        "DP-3,${config.stylix.image}"
+        "DP-5,${config.stylix.image}"
+        "HDMI-A-1,${config.stylix.image}"
+      ];
+    };
   };
 }
