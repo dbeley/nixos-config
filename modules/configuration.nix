a93d6a3d40d515adcf2c3e08401c8d0432726714
dbeley <6568955+dbeley@users.noreply.github.com>
2024-06-13 19:39:08 +0200

    nix: add back nix-path setting

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -160,6 +160,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
+      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
     };
     gc = {
       automatic = true;
