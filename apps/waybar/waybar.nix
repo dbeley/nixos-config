d74655058263d9f1d75dec6ea87531b88445827f
dbeley <6568955+dbeley@users.noreply.github.com>
2024-08-20 13:43:03 +0200

    waybar: add tooltip elements to clock and battery

--- apps/waybar/waybar.nix
+++ apps/waybar/waybar.nix
@@ -64,6 +64,7 @@
             "warning" = 30;
             "critical" = 15;
           };
+          "tooltip-format" = "{timeTo} ({power}W, {health}%)";
         };
         clock = {
           "interval" = 1;
@@ -71,6 +72,27 @@
           "format-alt" = "{:%H:%M}";
           "locale" = "fr_FR.UTF-8";
           "timezone" = "Europe/Paris";
+          "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+          "calendar" = {
+                    "mode"          = "year";
+                    "mode-mon-col"  = 3;
+                    "weeks-pos"     = "right";
+                    "on-scroll"     = 1;
+                    "format" = {
+                              "months"   =  "<span color='#ffead3'><b>{}</b></span>";
+                              "days"     =  "<span color='#ecc6d9'><b>{}</b></span>";
+                              "weeks"    =  "<span color='#99ffdd'><b>W{}</b></span>";
+                              "weekdays" =  "<span color='#ffcc66'><b>{}</b></span>";
+                              "today"    =  "<span color='#ff6699'><b><u>{}</u></b></span>";
+                              };
+                        };
+          "actions" =  {
+                    "on-click-right"= "mode";
+                    "on-click-forward"= "tz_up";
+                    "on-click-backward"= "tz_down";
+                    "on-scroll-up"= "shift_up";
+                    "on-scroll-down"= "shift_down";
+                    };
         };
         cpu = {
           "format" = "CPU {usage}%";
