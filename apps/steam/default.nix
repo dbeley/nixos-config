32ce18ffda4d7766f1393e27fe2596c455f5da03
dbeley <6568955+dbeley@users.noreply.github.com>
2024-08-30 15:39:20 +0200

    steam: delete unused package overrides

--- apps/steam/default.nix
+++ apps/steam/default.nix
@@ -1,25 +1,9 @@
-{ pkgs, ... }:
 {
   programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
-    # remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
-    # dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = true;
-    package = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
-        ];
-    };
   };
 }
