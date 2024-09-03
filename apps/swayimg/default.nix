a67542f2ac5f35a9be135114aa0dfd84c5909654
dbeley <6568955+dbeley@users.noreply.github.com>
2024-09-04 00:24:47 +0200

    nix fmt

--- apps/swayimg/default.nix
+++ apps/swayimg/default.nix
@@ -1,24 +1,24 @@
-{ pkgs, ... }:
-{
-   nixpkgs.overlays = [
-    (self: super: {
-      swayimg = super.swayimg.overrideAttrs (oldAttrs: {
-        version = "3.1";
-        src = super.fetchurl {
-          url ="https://github.com/artemsen/swayimg/archive/refs/tags/v3.1.tar.gz";
-          sha256 = "sha256-DGy3ES4WDhoppWbJLFt8b4xd7TSqC5QJPNTbw77qcGo=";
-        };
-      # nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
-      #   super.pkg-config
-      #   super.meson
-      # ];
-      # buildInputs = (oldAttrs.buildInputs or []) ++ [
-      #   super.giflib
-      #   super.libjpeg
-      #   super.libpng
-      #   super.libwebp
-      # ];
-      });
-    })
-  ];
-}
+# { pkgs, ... }:
+# {
+#   nixpkgs.overlays = [
+#     (self: super: {
+#       swayimg = super.swayimg.overrideAttrs (oldAttrs: {
+#         version = "3.1";
+#         src = super.fetchurl {
+#           url = "https://github.com/artemsen/swayimg/archive/refs/tags/v3.1.tar.gz";
+#           sha256 = "sha256-DGy3ES4WDhoppWbJLFt8b4xd7TSqC5QJPNTbw77qcGo=";
+#         };
+#         # nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
+#         #   super.pkg-config
+#         #   super.meson
+#         # ];
+#         # buildInputs = (oldAttrs.buildInputs or []) ++ [
+#         #   super.giflib
+#         #   super.libjpeg
+#         #   super.libpng
+#         #   super.libwebp
+#         # ];
+#       });
+#     })
+#   ];
+# }
--- apps/waybar/waybar.nix
+++ apps/waybar/waybar.nix
@@ -74,25 +74,25 @@
           "timezone" = "Europe/Paris";
           "tooltip-format" = "<tt><small>{calendar}</small></tt>";
           "calendar" = {
-                    "mode"          = "year";
-                    "mode-mon-col"  = 3;
-                    "weeks-pos"     = "right";
-                    "on-scroll"     = 1;
-                    "format" = {
-                              "months"   =  "<span color='#ffead3'><b>{}</b></span>";
-                              "days"     =  "<span color='#ecc6d9'><b>{}</b></span>";
-                              "weeks"    =  "<span color='#99ffdd'><b>W{}</b></span>";
-                              "weekdays" =  "<span color='#ffcc66'><b>{}</b></span>";
-                              "today"    =  "<span color='#ff6699'><b><u>{}</u></b></span>";
-                              };
-                        };
-          "actions" =  {
-                    "on-click-right"= "mode";
-                    "on-click-forward"= "tz_up";
-                    "on-click-backward"= "tz_down";
-                    "on-scroll-up"= "shift_up";
-                    "on-scroll-down"= "shift_down";
-                    };
+            "mode" = "year";
+            "mode-mon-col" = 3;
+            "weeks-pos" = "right";
+            "on-scroll" = 1;
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "mode";
+            "on-click-forward" = "tz_up";
+            "on-click-backward" = "tz_down";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
+          };
         };
         cpu = {
           "format" = "CPU {usage}%";
