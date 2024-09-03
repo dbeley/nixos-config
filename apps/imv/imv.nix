581ff575b3a8ab33532cf2f24b068d3152327777
dbeley <6568955+dbeley@users.noreply.github.com>
2024-09-04 00:24:39 +0200

    imv: add binding to delete current file

--- apps/imv/imv.nix
+++ apps/imv/imv.nix
@@ -7,6 +7,7 @@
       options.fullscreen = "true";
       binds."<bracketleft>" = "rotate by -90";
       binds."<bracketright>" = "rotate by 90";
+      binds."<Shift+Delete>" = "exec rm \"$imv_current_file\"; close";
     };
   };
 }
