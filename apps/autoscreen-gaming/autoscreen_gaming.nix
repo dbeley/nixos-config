d0a7d6816d7b93e570ca0e81091e1ecf09464cf2
dbeley <6568955+dbeley@users.noreply.github.com>
2024-09-30 19:33:20 +0200

    autoscreen: add gaming variant (take a screenshot randomly every 10 minutes in gameplay)

---
+++ apps/autoscreen-gaming/autoscreen_gaming.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+let
+  autoscreen_gaming = pkgs.writeShellScriptBin "autoscreen_gaming" ''
+    # Run hyprctl activewindow and get the output
+    active_window_info="$(hyprctl activewindow)"
+
+    # Extract the 'class' element using grep and awk
+    window_class="$(echo "$active_window_info" | grep -oP '(?<=class: ).*')"
+
+    # Check if the class is 'gamescope' or starts with 'steam_app_'
+    if [[ "$window_class" == "gamescope" || "$window_class" == steam_app_* ]]; then
+        TODAY="$(${pkgs.coreutils-full}/bin/date +%Y-%m-%d)"
+        DESTINATION_DIR="$HOME/Nextcloud/10-19_Images/11_Captures-d-écran/11.01_autoscreen/$TODAY"
+        ${pkgs.coreutils-full}/bin/mkdir -p "$DESTINATION_DIR"
+        ${pkgs.grim}/bin/grim "$DESTINATION_DIR/$(hostname)_nixos_autoscreen-gaming_$(${pkgs.coreutils-full}/bin/date +%Y-%m-%d_%H:%M:%S_%s).png"
+    fi
+  '';
+in
+{
+  home.packages = [ autoscreen_gaming ];
+
+  systemd.user.timers."autoscreen_gaming" = {
+    Unit = {
+      Description = "Run autoscreen_gaming every 10 minutes at random";
+      After = [ "multi-user.target" ];
+    };
+    Timer = {
+      OnCalendar = "*:0/10";
+      RandomizedDelaySec = 600;
+      AccuracySec = "1us";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
+
+  systemd.user.services."autoscreen_gaming" = {
+    Unit = {
+      Description = "Take a screenshot with grim";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${autoscreen_gaming}/bin/autoscreen_gaming";
+    };
+    Install = {
+      WantedBy = [ "multi-user.target" ];
+    };
+  };
+}
