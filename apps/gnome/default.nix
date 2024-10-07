89352ec9bb5c2e2eff5f7cb5c95f6e5329d6f32c
dbeley <6568955+dbeley@users.noreply.github.com>
2024-10-07 23:00:10 +0200

    gnome: fix fingerprint login

--- apps/gnome/default.nix
+++ apps/gnome/default.nix
@@ -17,4 +17,7 @@
 
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
+
+  # To fix login by fingerprint
+  security.pam.services.login.fprintAuth = false;
 }
