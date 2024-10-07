d7df4cc9117bd0f8a568dc496b6ad91a9080fc9b
dbeley <6568955+dbeley@users.noreply.github.com>
2024-10-07 22:59:37 +0200

    autoscreen: add gnome variant

---
+++ apps/autoscreen-gnome/autoscreen-gnome.nix
@@ -0,0 +1,41 @@
+{ pkgs, ... }:
+let
+  autoscreen_gnome = pkgs.writeShellScriptBin "autoscreen_gnome" ''
+    TODAY="$(${pkgs.coreutils-full}/bin/date +%Y-%m-%d)"
+    DESTINATION_DIR="$HOME/Nextcloud/10-19_Images/11_Captures-d-écran/11.01_autoscreen/$TODAY"
+
+    ${pkgs.coreutils-full}/bin/mkdir -p "$DESTINATION_DIR"
+    ${pkgs.flameshot}/bin/flameshot full -p "$DESTINATION_DIR/$(hostname)_nixos_autoscreen_$(${pkgs.coreutils-full}/bin/date +%Y-%m-%d_%H:%M:%S_%s).png"
+  '';
+in
+{
+  home.packages = [ autoscreen_gnome ];
+
+  systemd.user.timers."autoscreen_gnome" = {
+    Unit = {
+      Description = "Run autoscreen every hour at random (gnome-compatible)";
+      After = [ "multi-user.target" ];
+    };
+    Timer = {
+      OnCalendar = "hourly";
+      RandomizedDelaySec = 3600;
+      AccuracySec = "1us";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
+
+  systemd.user.services."autoscreen_gnome" = {
+    Unit = {
+      Description = "Take a screenshot with flameshot";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${autoscreen_gnome}/bin/autoscreen_gnome";
+    };
+    Install = {
+      WantedBy = [ "multi-user.target" ];
+    };
+  };
+}
