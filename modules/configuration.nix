86e8b50db83823b24c0a6d97b0263a05ee4f6e72
dbeley <6568955+dbeley@users.noreply.github.com>
2025-01-01 12:32:37 +0100

    deps: update flake.lock

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -45,7 +45,7 @@
   console.keyMap = "us-acentos";
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
