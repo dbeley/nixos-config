ce36107ddeb40354f8c48e2311a1bd700a7a5984
dbeley <6568955+dbeley@users.noreply.github.com>
2025-01-20 13:21:52 +0100

    vscode: add extensions

--- apps/vscode/vscode.nix
+++ apps/vscode/vscode.nix
@@ -11,11 +11,23 @@
       ms-python.vscode-pylance
       ms-azuretools.vscode-docker
       ms-python.debugpy
+      vue.volar
+      charliermarsh.ruff
     ];
     userSettings = {
       "files.autoSave" = "off";
       "editor.fontSize" = lib.mkForce 16;
       "editor.wordWrap" = "on";
+      "window.zoomLevel" = 2;
+      "[python]" = {
+        "editor.formatOnSave" = true;
+        "editor.codeActionsOnSave" = {
+          "source.fixAll" = "explicit";
+          "source.organizeImports" = "explicit";
+        };
+        "editor.defaultFormatter" = "charliermarsh.ruff";
+      };
+      "ruff.lineLength" = 120;
     };
   };
 }
