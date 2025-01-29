61d5ee80b03024f2bf50f0ffc044365287a5424e
dbeley <6568955+dbeley@users.noreply.github.com>
2025-01-29 15:31:04 +0100

    stylix: fix wallpaper urls

--- apps/stylix/default.nix
+++ apps/stylix/default.nix
@@ -13,35 +13,35 @@
     #   sha256 = "aR1JAQLBisLuz2Gd9ARV8qW0ePQqMpUiLq7teth2p4Q=";
     # };
     # HK plant
-    # image = pkgs.fetchurl {
-    #   url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/HK%20P9162201.jpg?raw=true";
-    #   sha256 = "+l73wncx2U79NQmBzjP0yVzUENqopIFjNzfOKTBxe2Q=";
-    # };
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/HK%20P9162201.jpg";
+      sha256 = "+l73wncx2U79NQmBzjP0yVzUENqopIFjNzfOKTBxe2Q=";
+    };
     # Taiwan road
     # image = pkgs.fetchurl {
-    #   url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/P9131591.jpg?raw=true";
+    #   url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/P9131591.jpg";
     #   sha256 = "rGs1PrdYZbtX0VrwTHPSfxNMjJ/9eVYcGpg09Jtg8Us=";
     # };
     # HK buildings
     # image = pkgs.fetchurl {
-    #   url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/HK%20P1011753.jpg?raw=true";
+    #   url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/HK%20P1011753.jpg";
     #   sha256 = "MUa09MnscDNCdwYO0pnTL5FyPnXd/hXLDVqZA9oh8zw=";
     # };
     # HK road
     # image = pkgs.fetchurl {
-    #   url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/HK%20P1010917.jpg?raw=true";
+    #   url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/HK%20P1010917.jpg";
     #   sha256 = "smPIXjBiSVcQiEdR1eXdIh069HQbZOWmc9uBu86VcSI=";
     # };
     # NYC hearst tower
     # image = pkgs.fetchurl {
-    #   url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/ET312096.jpg?raw=true";
+    #   url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/ET312096.jpg";
     #   sha256 = "mJe6WUXxb2eQBe8qsdDhxbfv7mUysjsKrhKrqFEp/xc=";
     # };
     # NYC 425 park avenue
-    image = pkgs.fetchurl {
-      url = "https://github.com/dbeley/photo-stream/blob/master/photos/original/ET012548.jpg?raw=true";
-      sha256 = "54yf34yXHRJj6pHlttoaXTXjP0AfvE8dMVyOO0ktH+g=";
-    };
+    # image = pkgs.fetchurl {
+    #   url = "https://raw.githubusercontent.com/dbeley/photo-stream/refs/heads/master/photos/original/ET012548.jpg";
+    #   sha256 = "54yf34yXHRJj6pHlttoaXTXjP0AfvE8dMVyOO0ktH+g=";
+    # };
 
     polarity = "dark";
     fonts = {
