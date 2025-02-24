9777442cdcd5faf5976028d3cdbddde38e6f111d
dbeley <6568955+dbeley@users.noreply.github.com>
2025-02-24 10:05:03 +0100

    vscode: update to latest upstream changes

--- apps/vscode/vscode.nix
+++ apps/vscode/vscode.nix
@@ -2,32 +2,34 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      mkhl.direnv
-      github.copilot
-      github.copilot-chat
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-azuretools.vscode-docker
-      ms-python.debugpy
-      vue.volar
-      charliermarsh.ruff
-    ];
-    userSettings = {
-      "files.autoSave" = "off";
-      "editor.fontSize" = lib.mkForce 16;
-      "editor.wordWrap" = "on";
-      "window.zoomLevel" = 2;
-      "[python]" = {
-        "editor.formatOnSave" = true;
-        "editor.codeActionsOnSave" = {
-          "source.fixAll" = "explicit";
-          "source.organizeImports" = "explicit";
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        mkhl.direnv
+        github.copilot
+        github.copilot-chat
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-azuretools.vscode-docker
+        ms-python.debugpy
+        vue.volar
+        charliermarsh.ruff
+      ];
+      userSettings = {
+        "files.autoSave" = "off";
+        "editor.fontSize" = lib.mkForce 16;
+        "editor.wordWrap" = "on";
+        "window.zoomLevel" = 2;
+        "[python]" = {
+          "editor.formatOnSave" = true;
+          "editor.codeActionsOnSave" = {
+            "source.fixAll" = "explicit";
+            "source.organizeImports" = "explicit";
+          };
+          "editor.defaultFormatter" = "charliermarsh.ruff";
         };
-        "editor.defaultFormatter" = "charliermarsh.ruff";
+        "ruff.lineLength" = 120;
       };
-      "ruff.lineLength" = 120;
     };
   };
 }
