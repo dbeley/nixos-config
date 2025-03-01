d3a02ea185598d71e683c17da18d65941a9ead6e
dbeley <6568955+dbeley@users.noreply.github.com>
2025-03-01 12:09:57 +0100

    helix: add language support

--- apps/helix/helix.nix
+++ apps/helix/helix.nix
@@ -2,6 +2,20 @@
 {
   programs.helix = {
     enable = true;
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+      }
+      {
+        name = "python";
+        auto-format = true;
+      }
+      {
+        name = "json";
+        auto-format = false;
+      }
+    ];
   };
   home.packages = with pkgs; [
     nil
