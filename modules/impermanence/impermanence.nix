048c1f29850c2c5ee882989373d15a0b2703ae0a
dbeley <6568955+dbeley@users.noreply.github.com>
2025-03-02 01:34:36 +0100

    impermanence: add more files to ignore

--- modules/impermanence/impermanence.nix
+++ modules/impermanence/impermanence.nix
@@ -7,41 +7,33 @@
       "Nextcloud"
       "Téléchargements"
       "nfs"
-      # {
-      #   directory = ".local/share/Steam";
-      #   # On a first install, disable symlink below then re-enable it after running Steam for the first time
-      #   # cf. https://github.com/nix-community/impermanence/issues/165
-      #   method = "symlink";
-      # }
       ".gnupg"
       ".ssh"
-      ".local/share/keyrings"
+      ".config/chromium"
+      ".config/gtk-3.0"
+      ".config/keepassxc"
+      ".config/libreoffice/4/user/pack"
+      ".config/mpdscrobble"
+      ".config/supersonic"
+      ".config/Nextcloud"
       ".local/share/direnv"
-      # ".steam"
+      # ".local/share/fish"
+      ".local/share/flatpak"
+      ".local/share/keyrings"
+      ".local/share/mpd"
+      # ".local/share/zoxide"
       ".mozilla/firefox/david"
       ".mozilla/native-messaging-hosts"
       # ".cache/mozilla/firefox/david"
-      ".config/chromium"
-      ".local/share/mpd"
-      ".config/Nextcloud"
-      ".config/keepassxc"
-      ".cache/keepassxc"
-      ".local/share/fish"
-      ".local/share/zoxide"
-      ".config/mpdscrobble"
-      ".config/supersonic"
+      # ".cache/keepassxc"
       ".cache/tealdeer/tldr-pages"
-      ".config/gtk-3.0"
-      ".config/libreoffice/4/user/pack"
-      ".local/share/flatpak"
       ".var/app"
     ];
     files = [
-      # Doesn't work
       # ".config/Nextcloud/cookies0.db"
-      # ".cache/keepassxc/keepassxc.ini"
-      # ".local/share/zoxide/db.zo"
-      # ".local/share/fish/fish_history"
+      ".cache/keepassxc/keepassxc.ini"
+      ".local/share/zoxide/db.zo"
+      ".local/share/fish/fish_history"
       ".cache/tofi-compgen"
       ".local/state/tofi-history"
     ];
