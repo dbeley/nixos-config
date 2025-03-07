9a93277cd5ef0de505ff53606bfcdd872cedbb0a
dbeley <6568955+dbeley@users.noreply.github.com>
2025-03-07 15:47:13 +0100

    overlay: disable zoom overlay

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -3,14 +3,14 @@
   # overlays that should apply to all hosts (to fix build, fix bugs, etc.)
   nixpkgs.overlays = [
     (self: super: {
-      # to fix zoom memory leak, working version found here https://github.com/NixOS/nixpkgs/pull/361097
-      zoom-us = super.zoom-us.overrideAttrs (oldAttrs: {
-        version = "6.2.11.5069";
-        src = super.fetchurl {
-          url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
-          sha256 = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
-        };
-      });
+      # # to fix zoom memory leak, working version found here https://github.com/NixOS/nixpkgs/pull/361097
+      # zoom-us = super.zoom-us.overrideAttrs (oldAttrs: {
+      #   version = "6.2.11.5069";
+      #   src = super.fetchurl {
+      #     url = "https://zoom.us/client/6.2.11.5069/zoom_x86_64.pkg.tar.xz";
+      #     sha256 = "sha256-k8T/lmfgAFxW1nwEyh61lagrlHP5geT2tA7e5j61+qw=";
+      #   };
+      # });
       # # to fix beets build, cf. https://github.com/NixOS/nixpkgs/pull/370234
       # beets = super.beets.overrideAttrs (oldAttrs: {
       #   patches = oldAttrs.patches ++ [
