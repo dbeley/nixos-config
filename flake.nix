b91f3c5c31dc97e427acee941fbe16255bf60c81
dbeley <6568955+dbeley@users.noreply.github.com>
2025-04-04 18:09:04 +0200

    deps: update flake.lock

--- flake.nix
+++ flake.nix
@@ -140,6 +140,7 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
