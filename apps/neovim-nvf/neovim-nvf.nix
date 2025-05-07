fc645896ae9fd5a209eb7afebff52a4ea630549c
dbeley <6568955+dbeley@users.noreply.github.com>
2025-05-07 16:25:46 +0200

    nvf: update config

--- apps/neovim-nvf/neovim-nvf.nix
+++ apps/neovim-nvf/neovim-nvf.nix
@@ -6,6 +6,29 @@
         viAlias = false;
         vimAlias = true;
         lsp.enable = true;
+        git.enable = true;
+
+        globals = {
+          mapleader = ";";
+        };
+
+        languages = {
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = true;
+          nix.enable = true;
+          python.enable = true;
+          bash.enable = true;
+          sql.enable = true;
+        };
+
+        autocomplete.nvim-cmp.enable = true;
+        dashboard.dashboard-nvim.enable = true;
+        filetree.neo-tree.enable = true;
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        treesitter.enable = true;
+        utility.motion.leap.enable = true;
       };
     };
   };
