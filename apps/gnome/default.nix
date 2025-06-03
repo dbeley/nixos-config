046a9082ffafea9bccfdd70918357244a72442ca
dbeley <6568955+dbeley@users.noreply.github.com>
2025-06-03 18:25:39 +0200

    gnome: adapt to new format

--- apps/gnome/default.nix
+++ apps/gnome/default.nix
@@ -6,14 +6,9 @@
       enable = true;
       user = "${user}";
     };
+    gdm.enable = true;
   };
-  services.xserver = {
-    enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager = {
-      gdm.enable = true;
-    };
-  };
+  services.desktopManager.gnome.enable = true;
 
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
