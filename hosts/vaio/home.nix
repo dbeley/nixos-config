0c6c7590e917c80e257700c37cefd8c9e0af020d
dbeley <6568955+dbeley@users.noreply.github.com>
2025-07-27 20:10:17 +0200

    vaio: fix niri monitor refresh rate override

--- hosts/vaio/home.nix
+++ hosts/vaio/home.nix
@@ -11,5 +11,5 @@
     supersonic
   ];
 
-  programs.niri.outputs."Dell Inc. DELL S2721DGF 6C1TR83".mode.refresh = lib.mkForce 59.951;
+  programs.niri.settings.outputs."Dell Inc. DELL S2721DGF 6C1TR83".mode.refresh = lib.mkForce 59.951;
 }
