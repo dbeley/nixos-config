181e204527512dcc444488b3c315b1a2cf57964f
dbeley <6568955+dbeley@users.noreply.github.com>
2025-07-27 19:58:20 +0200

    sunshine: fix common issues

--- apps/sunshine/default.nix
+++ apps/sunshine/default.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services.sunshine = {
     enable = true;
@@ -8,10 +9,31 @@
 
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
+    allowedTCPPorts = [
+      47984
+      47989
+      47990
+      48010
+    ];
     allowedUDPPortRanges = [
-      { from = 47998; to = 48000; }
-      { from = 8000; to = 8010; }
+      {
+        from = 47998;
+        to = 48000;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
     ];
   };
+
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
+
+  services.avahi.publish.enable = true;
+  services.avahi.publish.userServices = true;
 }
