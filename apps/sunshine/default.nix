e3643f8b457512c1a68edb4424670cbd5d70a999
dbeley <6568955+dbeley@users.noreply.github.com>
2025-07-29 14:51:04 +0200

    sunshine: remove useless code block

--- apps/sunshine/default.nix
+++ apps/sunshine/default.nix
@@ -27,13 +27,6 @@
     ];
   };
 
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
-
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
 }
