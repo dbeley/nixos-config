e8b6467eae6d926e013c2bc758bb4f4eeffb75e8
David BELEY <6568955+dbeley@users.noreply.github.com>
2025-08-02 15:46:55 +0000

    Disable hyprlock fingerprint on vaio

--- hosts/vaio/home.nix
+++ hosts/vaio/home.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  lib,
-  ...
-}:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
     audacity
@@ -12,4 +8,8 @@
   ];
 
   programs.niri.settings.outputs."Dell Inc. DELL S2721DGF 6C1TR83".mode.refresh = lib.mkForce 59.951;
+
+  # Disable fingerprint authentication in hyprlock to avoid
+  # wake-from-sleep issues on the VAIO host.
+  programs.hyprlock.settings.auth.fingerprint.enabled = lib.mkForce false;
 }
