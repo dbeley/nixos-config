2d9a57c482df633b15901d135d1c8c75ab1dc984
dbeley <6568955+dbeley@users.noreply.github.com>
2025-08-03 11:26:17 +0200

    vaio: add apps

--- hosts/vaio/home.nix
+++ hosts/vaio/home.nix
@@ -7,9 +7,12 @@
   home.packages = with pkgs; [
     audacity
     beets
+    musescore
     nautilus
+    shotcut
     supersonic
   ];
 
   programs.niri.settings.outputs."Dell Inc. DELL S2721DGF 6C1TR83".mode.refresh = lib.mkForce 59.951;
+  programs.hyprlock.settings.auth.fingerprint.enabled = lib.mkForce false;
 }
