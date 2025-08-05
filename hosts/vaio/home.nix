2396d7a58ffa193c1a504e9e569c9dc3fc24ae94
dbeley <6568955+dbeley@users.noreply.github.com>
2025-08-05 02:01:23 +0200

    vaio: disable hypridle

--- hosts/vaio/home.nix
+++ hosts/vaio/home.nix
@@ -15,4 +15,5 @@
 
   programs.niri.settings.outputs."Dell Inc. DELL S2721DGF 6C1TR83".mode.refresh = lib.mkForce 59.951;
   programs.hyprlock.settings.auth.fingerprint.enabled = lib.mkForce false;
+  services.hypridle.enable = lib.mkForce false;
 }
