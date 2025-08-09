51c9a5f1a7e851f718e0e566636bf3c265c9a70b
dbeley <6568955+dbeley@users.noreply.github.com>
2025-08-09 19:21:56 +0200

    laptop: add tlp

--- modules/common/laptop.nix
+++ modules/common/laptop.nix
@@ -1,5 +1,19 @@
 {
   powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT0 = 95;
+      };
+    };
+  };
   hardware.bluetooth = {
     enable = true;
   };
