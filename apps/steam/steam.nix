7d88f135acf02210608eaa3a527c76c2d378886c
David BELEY <6568955+dbeley@users.noreply.github.com>
2025-10-02 21:35:47 +0000

    Enhance MangoHud overlay metrics

--- apps/steam/steam.nix
+++ apps/steam/steam.nix
@@ -6,9 +6,16 @@
       horizontal = true;
       battery = true;
       cpu_stats = true;
-      gpu_stats = true;
+      cpu_load = true;
+      cpu_temp = true;
       cpu_power = true;
+      gpu_stats = true;
+      gpu_load = true;
+      gpu_temp = true;
+      gpu_core_clock = true;
+      gpu_mem_clock = true;
       gpu_power = true;
+      vram = true;
       ram = true;
       fps = true;
       table_columns = 14;
