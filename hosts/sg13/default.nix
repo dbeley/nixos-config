ba5c36ee2c8bf94bcef83fdf43b965c2c564c74d
dbeley <6568955+dbeley@users.noreply.github.com>
2025-10-04 15:41:50 +0200

    sg13: disable ryzen eco mode

--- hosts/sg13/default.nix
+++ hosts/sg13/default.nix
@@ -5,9 +5,6 @@
 }:
 let
   gpuPowerLimitWatts = 180;
-  pptLimitWatts = 88;
-  tdcLimitAmps = 60;
-  edcLimitAmps = 90;
 
   nvidiaPowerLimitScript = pkgs.writeShellScript "sg13-nvidia-power-limit" ''
     #!${pkgs.bash}/bin/bash
@@ -33,39 +30,14 @@ let
     /run/current-system/sw/bin/nvidia-smi -pl ${toString gpuPowerLimitWatts}
   '';
 
-  ryzenEcoScript = pkgs.writeShellScript "sg13-ryzen-eco" ''
-    #!${pkgs.bash}/bin/bash
-    set -euo pipefail
-
-    ${lib.getExe pkgs.ryzenadj} \
-      --ppt-limit=${toString pptLimitWatts} \
-      --tdc-limit=${toString tdcLimitAmps} \
-      --edc-limit=${toString edcLimitAmps} \
-      --coall=-15
-  '';
-
 in
 {
-  environment.systemPackages = [ pkgs.ryzenadj ];
-
-  boot.kernelModules = [ "msr" ];
-
   systemd.services.sg13-nvidia-power-limit = {
     description = "Set NVIDIA GPU power limit for sg13";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = nvidiaPowerLimitScript;
-      RemainAfterExit = true;
-    };
-  };
-
-  systemd.services.sg13-ryzen-eco = {
-    description = "Apply Ryzen 5950X eco tuning";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = ryzenEcoScript;
       RemainAfterExit = true;
     };
   };
