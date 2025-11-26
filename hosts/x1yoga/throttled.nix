144ddcb42e7aa9dda24f7697c3c3c5da56757f13
dbeley <6568955+dbeley@users.noreply.github.com>
2025-11-26 19:12:31 +0100

    x1yoga: update throttled config

--- hosts/x1yoga/throttled.nix
+++ hosts/x1yoga/throttled.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -13,35 +12,35 @@ Autoreload: True
 
 [BATTERY]
 Update_Rate_s: 30
-PL1_Tdp_W: 25
+PL1_Tdp_W: 20
 PL1_Duration_s: 28
-PL2_Tdp_W: 35
-PL2_Duration_S: 0.002
+PL2_Tdp_W: 25
+PL2_Duration_S: 28
 Trip_Temp_C: 85
 cTDP: 0
 Disable_BDPROCHOT: False
 
 [AC]
-Update_Rate_s: 5
-PL1_Tdp_W: 35
-PL1_Duration_s: 30
-PL2_Tdp_W: 55
-PL2_Duration_S: 0.002
-Trip_Temp_C: 95
+Update_Rate_s: 30
+PL1_Tdp_W: 20
+PL1_Duration_s: 28
+PL2_Tdp_W: 25
+PL2_Duration_S: 28
+Trip_Temp_C: 90
 cTDP: 0
 Disable_BDPROCHOT: False
 
 [UNDERVOLT.BATTERY]
-CORE: -50
+CORE: -140
 GPU: -50
-CACHE: -50
+CACHE: -100
 UNCORE: -50
 ANALOGIO: 0
 
 [UNDERVOLT.AC]
-CORE: -50
+CORE: -140
 GPU: -50
-CACHE: -50
+CACHE: -100
 UNCORE: -50
 ANALOGIO: 0
   '';
