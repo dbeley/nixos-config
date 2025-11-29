a33bdb3b128a73dc36695e2580a0dcca05ed8fae
dbeley <6568955+dbeley@users.noreply.github.com>
2025-11-29 16:33:46 +0100

    helix: fix config

--- apps/helix/helix.nix
+++ apps/helix/helix.nix
@@ -20,30 +20,15 @@
       editor = {
         cursorline = true;
         cursorcolumn = false;
-        gutters = [ "diagnostics" "spacer" "line-numbers" "spacer" ];
         auto-completion = true;
         auto-format = true;
         auto-save = false;
-        statusline = {
-          left = [ "mode" "spinner" "file-name" ];
-          center = [ "diagnostics" ];
-          right = [ "position" "total-line-numbers" "file-type" "version-control" ];
-        };
         scrolloff = 5;
         auto-pairs = true;
-        whitespace = {
-          render = "all";
-          characters = {
-            space = "·";
-            nbsp = "⍽";
-            tab = "⇥";
-          };
-        };
       };
     };
   };
   home.packages = with pkgs; [
-    # nil
     nixd
   ];
 }
