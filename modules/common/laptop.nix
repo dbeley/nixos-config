331573c27597263d6844e9154eee4d2cfbc2b029
dbeley <6568955+dbeley@users.noreply.github.com>
2025-12-01 18:16:36 +0100

    tuned: replace tlp by tuned for all laptops

--- modules/common/laptop.nix
+++ modules/common/laptop.nix
@@ -1,18 +1,17 @@
 {
-  powerManagement.enable = true;
-  powerManagement.powertop.enable = true;
+  powerManagement = {
+    enable = true;
+    powertop.enable = false;
+    cpuFreqGovernor = "schedutil";
+  };
   services = {
     power-profiles-daemon.enable = false;
-    tlp = {
+    upower.enable = true;
+    tuned = {
       enable = true;
       settings = {
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        STOP_CHARGE_THRESH_BAT0 = 95;
-        USB_AUTOSUSPEND = "1";
-        USB_DENYLIST = "25a7:fa7b";
+        sleep_interval = 20;
+        update_interval = 60;
       };
     };
   };
