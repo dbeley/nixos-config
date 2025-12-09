4400787e41256b5491ed28c8c363e7cdfed965ff
dbeley <6568955+dbeley@users.noreply.github.com>
2025-12-09 13:53:52 +0100

    latitude: update throttled config

--- hosts/latitude/throttled.nix
+++ hosts/latitude/throttled.nix
@@ -7,20 +7,20 @@
     Autoreload: True
 
     [BATTERY]
-    Update_Rate_s: 30
-    PL1_Tdp_W: 10
+    Update_Rate_s: 10
+    PL1_Tdp_W: 15
     PL1_Duration_s: 28
-    PL2_Tdp_W: 20
+    PL2_Tdp_W: 25
     PL2_Duration_S: 28
     Trip_Temp_C: 85
     cTDP: 0
     Disable_BDPROCHOT: False
 
     [AC]
-    Update_Rate_s: 30
-    PL1_Tdp_W: 40
+    Update_Rate_s: 10
+    PL1_Tdp_W: 25
     PL1_Duration_s: 28
-    PL2_Tdp_W: 40
+    PL2_Tdp_W: 30
     PL2_Duration_S: 28
     Trip_Temp_C: 85
     cTDP: 0
