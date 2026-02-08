83955b61e0ffc11282ecf9365f9377a316016279
dbeley <6568955+dbeley@users.noreply.github.com>
2026-02-08 22:30:13 +0100

    android: add udev rules for lineageos dev

--- apps/android/default.nix
+++ apps/android/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.android-tools ];
+  environment.systemPackages = [
+    pkgs.android-tools
+  ];
+  services.udev.extraRules = ''
+    # Samsung (vendor 04e8) and Motorola (vendor 22b8) adb access
+    SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", MODE="0666"
+  '';
 }
