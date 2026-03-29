aca359d7fe641857a2a7e5680434f5466fac4d9f
dbeley <6568955+dbeley@users.noreply.github.com>
2026-03-29 20:50:59 +0200

    helix: add common lsps

--- apps/helix/helix.nix
+++ apps/helix/helix.nix
@@ -2,20 +2,80 @@
 {
   programs.helix = {
     enable = true;
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-      }
-      {
-        name = "python";
-        auto-format = true;
-      }
-      {
-        name = "json";
-        auto-format = false;
-      }
-    ];
+    languages = {
+      language-server = {
+        pyright = {
+          command = "${pkgs.pyright}/bin/pyright-langserver";
+          args = [ "--stdio" ];
+        };
+        ruff = {
+          command = "${pkgs.ruff}/bin/ruff";
+          args = [ "server" ];
+        };
+        rust-analyzer = {
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+          config = {
+            check.command = "clippy";
+            cargo.features = "all";
+          };
+        };
+        nixd = {
+          command = "${pkgs.nixd}/bin/nixd";
+        };
+        command = "${pkgs.vscode-langservers-extracted}/bin/vscode-json-language-server";
+        args = [ "--stdio" ];
+        config.provideFormatter = true;
+      };
+      marksman = {
+        command = "${pkgs.marksman}/bin/marksman";
+        args = [ "server" ];
+      };
+      language = [
+        {
+          name = "python";
+          auto-format = true;
+          language-servers = [
+            "pyright"
+            "ruff"
+          ];
+          formatter = {
+            command = "${pkgs.ruff}/bin/ruff";
+            args = [
+              "format"
+              "--quiet"
+              "-"
+            ];
+          };
+        }
+        {
+          name = "rust";
+          auto-format = true;
+          language-servers = [ "rust-analyzer" ];
+        }
+        {
+          name = "nix";
+          auto-format = true;
+          language-servers = [ "nixd" ];
+          formatter = {
+            command = "${pkgs.nixfmt}/bin/nixfmt";
+          };
+        }
+        {
+          name = "json";
+          auto-format = false;
+          language-servers = [ "vscode-json-language-server" ];
+        }
+        {
+          name = "csv";
+          auto-format = false;
+        }
+        {
+          name = "markdown";
+          auto-format = false;
+          language-servers = [ "marksman" ];
+        }
+      ];
+    };
     settings = {
       editor = {
         cursorline = true;
@@ -29,6 +89,12 @@
     };
   };
   home.packages = with pkgs; [
+    pyright
+    ruff
+    rust-analyzer
     nixd
+    vscode-langservers-extracted
+    marksman
+    nixfmt
   ];
 }
