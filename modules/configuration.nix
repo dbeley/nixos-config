598739eed8816c7051eb0fc121212ac3a1e68bff
dbeley <6568955+dbeley@users.noreply.github.com>
2026-04-05 22:33:20 +0200

    deps: update flake.lock

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -170,17 +170,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   nix = {
     settings = {
       experimental-features = [
