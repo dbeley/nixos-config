e64c85630f971c3177ed3bdb8d77321c1223b125
dbeley <6568955+dbeley@users.noreply.github.com>
2026-04-17 16:59:05 +0200

    impermanence: convert btrfs cleanup script to systemd service

--- modules/impermanence/default.nix
+++ modules/impermanence/default.nix
@@ -1,6 +1,7 @@
 {
-  lib,
   hostName,
+  lib,
+  pkgs,
   user,
   ...
 }:
@@ -132,30 +133,70 @@
     "/nix".neededForBoot = true;
   };
 
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/mapper/root_vg_${hostName} /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/persistent/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/persistent/old_roots/$timestamp/"
-    fi
+  boot.initrd.systemd = {
+    services.impermanence-btrfs-rolling-root = {
+      description = "Archiving existing BTRFS root subvolume and creating a fresh one";
+      # Specify dependencies explicitly
+      unitConfig.DefaultDependencies = false;
+      # The script needs to run to completion before this service is done
+      serviceConfig = {
+        Type = "oneshot";
+        # NOTE: to be able to see errors in your script do this
+        StandardOutput = "journal+console";
+        StandardError = "journal+console";
+      };
+      # This service is required for boot to succeed
+      requiredBy = [ "initrd.target" ];
+      # Should complete before any file systems are mounted
+      before = [ "sysroot.mount" ];
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+      # Wait until the root device is available
+      # If you're altering a different device, specify its device unit explicitly.
+      # see: systemd-escape(1)
+      requires = [ "initrd-root-device.target" ];
+      after = [
+        "initrd-root-device.target"
+        # Allow hibernation to resume before trying to alter any data
+        "local-fs-pre.target"
+      ];
+
+      # The body of the script. Make your changes to data here
+      script = ''
+        mkdir /btrfs_tmp
+        mount /dev/mapper/root_vg_${hostName} /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/persistent/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/persistent/old_roots/$timestamp"
+        fi
+
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /btrfs_tmp/persistent/old_roots/ -mindepth 1 -maxdepth 1 -mtime +1); do
+            delete_subvolume_recursively "$i"
         done
-        btrfs subvolume delete "$1"
-    }
 
-    for i in $(find /btrfs_tmp/persistent/old_roots/ -mindepth 1 -maxdepth 1 -mtime +1); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
+    extraBin = {
+      # "mkfs.ext4" = "${pkgs.e2fsprogs}/bin/mkfs.ext4";
+      "mkdir" = "${pkgs.coreutils}/bin/mkdir";
+      "date" = "${pkgs.coreutils}/bin/date";
+      "stat" = "${pkgs.coreutils}/bin/stat";
+      "mv" = "${pkgs.coreutils}/bin/mv";
+      "find" = "${pkgs.findutils}/bin/find";
+      "btrfs" = "${pkgs.btrfs-progs}/bin/btrfs";
+      # mount & umount already exist
+    }; # NOTE: path = [...]; doesnt work for initrd, use full paths in your script or extraBin
+  };
 
   # Temporary workaround for Téléchargements folder, cf. https://github.com/nix-community/impermanence/issues/290
   systemd.mounts = [
