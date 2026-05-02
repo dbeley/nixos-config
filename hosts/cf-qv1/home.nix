f04bd7a663040a16b9e5d6bfccdf32cbf5a37a8e
dbeley <6568955+dbeley@users.noreply.github.com>
2026-05-02 11:27:50 +0200

    cf-qv1: increase touchpad sensitivity

--- hosts/cf-qv1/home.nix
+++ hosts/cf-qv1/home.nix
@@ -15,12 +15,15 @@
     # supersonic
   ];
 
+  # CF-QV1 specific settings
+  # 3880x1920 screen needs higher scaling and font size
   wayland.windowManager.niri.settings.output = lib.mkForce [
     {
       _args = [ "eDP-1" ];
       scale = 2.0;
     }
   ];
-
   programs.hyprlock.settings.label.font_size = lib.mkForce 100;
+  # Increase trackpad sensitivity for the smaller trackpad
+  wayland.windowManager.niri.settings.input.touchpad."accel-speed" = 0.4;
 }
