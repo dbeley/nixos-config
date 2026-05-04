2445bc27c0b51ea8c5a4cdbeaeff9df53d9f4e63
dbeley <6568955+dbeley@users.noreply.github.com>
2026-05-04 05:42:10 +0200

    tmux: convert configuration to declarative as much as possible

--- apps/tmux/tmux.nix
+++ apps/tmux/tmux.nix
@@ -1,8 +1,13 @@
 { lib, ... }:
 {
-  xdg.configFile."tmux/tmuxline.conf".source = ./tmuxline.conf;
   programs.tmux = {
     enable = true;
+    terminal = "tmux-256color";
+    baseIndex = 1;
+    keyMode = "vi";
+    historyLimit = 999999;
+    escapeTime = 0;
+    clock24 = true;
     extraConfig = lib.fileContents ./tmux.conf;
   };
 }
