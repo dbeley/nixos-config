67f625fe726bd703fcac46080c906b4ac7c434f4
dbeley <dbeley@users.noreply.github.com>
2026-07-04 06:58:36 +0200

    fix: open firewall for cairn direct-access ports (9090, 9091)

--- hosts/default.nix
+++ hosts/default.nix
@@ -583,7 +583,7 @@ in
             port = 8080;  # Hermes is on port 80
           };
         };
-        networking.firewall.allowedTCPPorts = [ 8080 ];
+        networking.firewall.allowedTCPPorts = [ 8080 9090 9091 ];
       }
     ];
   };
