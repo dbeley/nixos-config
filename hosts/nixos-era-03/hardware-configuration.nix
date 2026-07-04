5155a19db5bcbc34de4b18d9af867c548d732009
dbeley <6568955+dbeley@users.noreply.github.com>
2026-07-04 10:59:34 +0200

    nixos-era-03: adjust device id

--- hosts/nixos-era-03/hardware-configuration.nix
+++ hosts/nixos-era-03/hardware-configuration.nix
@@ -15,7 +15,7 @@
   };
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d8d88e75-0e97-47c5-bfdc-011a77ec1d12";
+    device = "/dev/disk/by-uuid/7dab74fd-37a0-4c8f-8e2e-baad05fad55e";
     fsType = "ext4";
   };
   swapDevices = [ ];
