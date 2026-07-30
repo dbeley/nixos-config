3648ef85e8d10d23d83d8b4ff41bae94aca4331c
dbeley <6568955+dbeley@users.noreply.github.com>
2026-07-31 00:05:17 +0200

    covertone: adjust settings

--- apps/covertone/default.nix
+++ apps/covertone/default.nix
@@ -5,8 +5,5 @@
   services.covertone = {
     enable = true;
     virtualHost = "covertone.navidrome.home";
-    server = "http://navidrome.home";
-    username = "alice";
-    password = "...";
   };
 }
