8c570912d4b061343fe11b9542e19b8a98ab60d0
dbeley <6568955+dbeley@users.noreply.github.com>
2026-08-02 18:40:30 +0200

    cf-qv1: fix niri UI scaling

--- hosts/cf-qv1/home.nix
+++ hosts/cf-qv1/home.nix
@@ -18,17 +18,28 @@
 
   # CF-QV1 specific settings
   # 3880x1920 screen needs higher scaling and font size
-  # Use extraConfig for output overrides since they're in _children for toKDL compatibility
-  wayland.windowManager.niri.extraConfig = ''
-    output "DP-4" {
-      mode "2560x1440@143.912"
-      position x=1440 y=0
+  wayland.windowManager.niri.settings._children = lib.mkBefore [
+    {
+      output = {
+        _args = [ "DP-4" ];
+        mode = "2560x1440@143.912";
+        position._props = {
+          x = 1440;
+          y = 0;
+        };
+      };
     }
-    output "eDP-1" {
-      scale 2.0
-      position x=0 y=0
+    {
+      output = {
+        _args = [ "eDP-1" ];
+        scale = 2.0;
+        position._props = {
+          x = 0;
+          y = 0;
+        };
+      };
     }
-  '';
+  ];
   programs.hyprlock.settings.label.font_size = lib.mkForce 100;
   # Increase trackpad sensitivity for the smaller trackpad
   wayland.windowManager.niri.settings.input.touchpad = {
