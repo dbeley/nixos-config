13c6c76241270ebfbddc226a0782e3592f829c21
dbeley <6568955+dbeley@users.noreply.github.com>
2026-08-03 11:47:21 +0200

    restic: generic aliases to allow the use of other repos

--- apps/restic/restic.nix
+++ apps/restic/restic.nix
@@ -7,18 +7,34 @@
   ];
 
   programs.fish = {
-    shellAliases = {
-      backup-init = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password init";
-      backup-status = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password snapshots";
-      backup-restore = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password restore latest --target ~/restic-restore";
-      backup-forget = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 3 --prune";
-      backup-check = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password check";
-      backup-unlock = "restic -r rclone:onedrive:backup --password-file ~/.config/restic/password unlock";
-      rclone-config = "rclone config";
-    };
-
     functions = {
+      backup-repo = ''
+        if set -q argv[1]
+          echo $argv[1]
+        else
+          echo rclone:onedrive:backup
+        end
+      '';
+      backup-init = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password init
+      '';
+      backup-status = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password snapshots
+      '';
+      backup-restore = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password restore latest --target ~/restic-restore
+      '';
+      backup-forget = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 3 --prune
+      '';
+      backup-check = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password check
+      '';
+      backup-unlock = ''
+        restic -r (backup-repo $argv) --password-file ~/.config/restic/password unlock
+      '';
       backup-run = ''
+        set repo (backup-repo $argv)
         set folders_file ~/.config/restic/backup-folders.txt
 
         if not test -f $folders_file
@@ -34,7 +50,7 @@
         for folder in (cat $folders_file | grep -v '^#' | grep -v '^$')
           set expanded_folder (eval echo $folder)
           echo "Backing up: $expanded_folder"
-          restic -r rclone:onedrive:backup --verbose --password-file ~/.config/restic/password backup $expanded_folder
+          restic -r $repo --verbose --password-file ~/.config/restic/password backup $expanded_folder
         end
       '';
     };
