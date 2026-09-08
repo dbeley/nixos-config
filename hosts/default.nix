5a27d8f5c9382a70fd239b3feca757d5cf2d2aca
dbeley <6568955+dbeley@users.noreply.github.com>
2026-09-08 23:57:23 +0200

    cf-qv1: delete throttled

--- hosts/default.nix
+++ hosts/default.nix
@@ -506,8 +506,6 @@ in
       }
       ../modules/common/laptop-thermald.nix
       ../modules/common/screen-rotation.nix
-      ../modules/hardware/throttled.nix
-      ../hosts/cf-qv1/throttled.nix
     ];
   };
   x1yoga = mkHost {
